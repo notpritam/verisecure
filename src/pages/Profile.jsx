5dfe604d74d6793c5912a63b81d2afef4c1dabd0
Pritam Sharma <notpritamsharma@gmail.com>
2023-12-09 13:58:35 +0530

    User Uploads

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,20 @@ import ethLogo from "../assets/ethIndia.svg";
 import Header from "@/components/header";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Github, Upload, UserRoundCog, Wallet } from "lucide-react";
+import {
+  Github,
+  Lock,
+  ShieldEllipsis,
+  Upload,
+  UserRoundCog,
+  View,
+  Wallet,
+} from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React, { useState } from "react";
+import { cn } from "@/lib/utils";
 
 function Profile() {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -21,9 +30,11 @@ function Profile() {
     <>
       <header className="flex z-[999] items-center border-b-2 pb-4 backdrop-blur-xl fixed top-0 left-0 right-0 pl-[4rem] pr-[4rem] p-4 dark justify-between">
         <div className="flex items-center justify-between w-full">
-          <span className="text-[1.5rem] tracking-wide font-bold">
-            AnonVerify
-          </span>
+          <Link to={"/"}>
+            <span className="text-[1.5rem] tracking-wide font-bold">
+              AnonVerify
+            </span>
+          </Link>
           <div className="flex gap-4">
             <Link to={"/profile?action=upload"}>
               {/* <Button className="flex gap-2">
@@ -42,7 +53,7 @@ function Profile() {
       </header>
 
       <div className="w-full h-full flex flex-col mt-[4rem] justify-center items-center">
-        <div className="min-w-screen justify-center flex p-[2rem] min-h-screen w-full max-w-[1280px] h-full items-center">
+        <div className="min-w-screen justify-center flex p-[2rem]  w-full max-w-[1280px] h-full items-center">
           <Card className="p-6 items-center flex flex-col gap-4 justify-center w-full">
             <Label htmlFor="picture" className="text-[1.25rem]">
               Choose the file
@@ -61,12 +72,153 @@ function Profile() {
             )}
           </Card>
         </div>
-        <div className="w-full max-w-[1280px]">
-          {Array.from({ length: 10 }).map((_, index) => (
-            <>
-              <Card className="flex gap-4 p-2 pl-6 pr-6 w-full justify-between max-w-[600px] items-center"></Card>
-            </>
-          ))}
+        <div className="flex gap-4">
+          <div className="w-full flex flex-col gap-4 max-w-[1280px]">
+            <span className="text-[2rem] font-bold">Your Files</span>
+            {Array.from({ length: 10 }).map((_, i) => (
+              <>
+                <Card
+                  key={i}
+                  className={cn(
+                    "flex gap-4 p-2 pl-6 pr-6 w-full justify-between max-w-[600px] items-center"
+                  )}
+                >
+                  <div className="flex gap-4 items-center">
+                    <img
+                      className={cn("h-10 w-10 rounded-full")}
+                      src="https://i.pravatar.cc/300"
+                    ></img>
+                    <div className="flex flex-col">
+                      <span className={cn("text-[1.5rem] font-bold")}>
+                        John Doe {i}
+                      </span>
+                      <div className="flex gap-8">
+                        <span className="text-[0.8rem] opacity-80">
+                          0x1234567890
+                        </span>
+                        <span className="text-[0.8rem] opacity-80">
+                          Size :- 123Kb
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex gap-4">
+                    {i % 2 == 0 ? (
+                      <>
+                        <Button
+                          // onClick={() => onClickVersion(data)}
+                          variant="outline"
+                          className="flex gap-2 items-center "
+                        >
+                          {/* <Eye /> */}
+                          <Lock />
+                          Request Access
+                        </Button>
+                      </>
+                    ) : i % 3 == 0 ? (
+                      <>
+                        {" "}
+                        <Button
+                          // onClick={() => onClick(data)}
+                          variant="outline"
+                          className="flex gap-2 items-center "
+                        >
+                          {/* <Eye /> */}
+                          <ShieldEllipsis />
+                          Pending Access
+                        </Button>
+                      </>
+                    ) : (
+                      <Button
+                        //   onClick={() => handleShowDetails(i)}
+                        variant="outline"
+                        className="flex gap-2 items-center "
+                      >
+                        {/* <Eye /> */}
+                        {/* <ShieldEllipsis /> */}
+                        <View color="#262e9c" />
+                        Already Viewed
+                      </Button>
+                    )}
+                    {/* <Button variant="outline">Verify</Button> */}
+                  </div>
+                </Card>
+              </>
+            ))}
+          </div>
+          <div className="w-full flex flex-col gap-4 max-w-[1280px]">
+            <span className="text-[2rem] font-bold">Viewed Files</span>
+            {Array.from({ length: 10 }).map((_, i) => (
+              <>
+                <Card
+                  key={i}
+                  className={cn(
+                    "flex gap-4 p-2 pl-6 pr-6 w-full justify-between max-w-[600px] items-center"
+                  )}
+                >
+                  <div className="flex gap-4 items-center">
+                    <img
+                      className={cn("h-10 w-10 rounded-full")}
+                      src="https://i.pravatar.cc/300"
+                    ></img>
+                    <div className="flex flex-col">
+                      <span className={cn("text-[1.5rem] font-bold")}>
+                        John Doe {i}
+                      </span>
+                      <div className="flex gap-8">
+                        <span className="text-[0.8rem] opacity-80">
+                          0x1234567890
+                        </span>
+                        <span className="text-[0.8rem] opacity-80">
+                          Size :- 123Kb
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex gap-4">
+                    {i % 2 == 0 ? (
+                      <>
+                        <Button
+                          // onClick={() => onClickVersion(data)}
+                          variant="outline"
+                          className="flex gap-2 items-center "
+                        >
+                          {/* <Eye /> */}
+                          <Lock />
+                          Request Access
+                        </Button>
+                      </>
+                    ) : i % 3 == 0 ? (
+                      <>
+                        {" "}
+                        <Button
+                          // onClick={() => onClick(data)}
+                          variant="outline"
+                          className="flex gap-2 items-center "
+                        >
+                          {/* <Eye /> */}
+                          <ShieldEllipsis />
+                          Pending Access
+                        </Button>
+                      </>
+                    ) : (
+                      <Button
+                        //   onClick={() => handleShowDetails(i)}
+                        variant="outline"
+                        className="flex gap-2 items-center "
+                      >
+                        {/* <Eye /> */}
+                        {/* <ShieldEllipsis /> */}
+                        <View color="#262e9c" />
+                        Already Viewed
+                      </Button>
+                    )}
+                    {/* <Button variant="outline">Verify</Button> */}
+                  </div>
+                </Card>
+              </>
+            ))}
+          </div>
         </div>
       </div>
     </>
