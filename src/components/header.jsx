b144d1f758436997018c9da3dd8143868ebb4752
Pritam Sharma <notpritamsharma@gmail.com>
2023-12-09 21:57:21 +0530

    Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { Upload, UserRoundCog, Wallet, Wallet2 } from "lucide-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, redirect, useLocation } from "react-router-dom";
 
 function Header() {
   const location = useLocation();
 
+  const isUserLoggedIn = false;
+
   React.useEffect(() => {
-    console.log(location.pathname, "we are at this url"); // prints the current path
+    console.log(location.pathname, "we are at this url");
+
+    if (location.pathname != "/" && isUserLoggedIn == false) {
+      // redirect the user to login page to login or open wallet login popup
+    } // prints the current path
   }, [location]);
 
   // also for to checking the authorization to where to redirect the user we can
   // declare that function here in header, this component is used in all the pages so it will bydefault check everytime it get renders
   return (
-    <header className="flex border-white z-[999] items-center border-b-2 pb-4 backdrop-blur-xl fixed top-0 left-0 right-0 pl-[4rem] pr-[4rem] p-4 dark justify-between">
+    <header className="flex border-white z-[999]  items-center border-b-2 pb-4 backdrop-blur-xl fixed top-0 left-0 right-0 pl-[4rem] pr-[4rem] p-4 dark justify-between">
       <div className="flex items-center justify-between w-full">
         <Link to={"/"}>
           <span className="text-[1.5rem] tracking-wide font-bold">
@@ -22,28 +28,42 @@ function Header() {
         </Link>
 
         <div className="flex gap-4">
-          <Link to={"/profile?action=upload"}>
-            <Button className="flex gap-2">
-              <Upload />
-              Upload Files
-            </Button>
-          </Link>
-          <Link to="/profile">
-            <Button variant="outline" className="flex gap-2">
-              <UserRoundCog />
-              Your Profile
-            </Button>
-          </Link>
+          {location.pathname === "/profile" ? (
+            <>
+              <Link to={"/profile?action=upload"}>
+                <Button className="flex gap-2">
+                  <Upload />
+                  Upload Files
+                </Button>
+              </Link>
+            </>
+          ) : (
+            ""
+          )}
 
-          <Button
-            onClick={() => {
-              // handle login here
-            }}
-            className="flex gap-2"
-          >
-            <Wallet2 />
-            Connect Wallet
-          </Button>
+          {isUserLoggedIn ? (
+            <>
+              {" "}
+              <Link to="/profile">
+                <Button variant="outline" className="flex gap-2">
+                  <UserRoundCog />
+                  Your Profile
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={() => {
+                  // handle login here
+                }}
+                className="flex gap-2"
+              >
+                <Wallet2 />
+                Connect Wallet
+              </Button>
+            </>
+          )}
         </div>
       </div>
     </header>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -35,27 +35,6 @@ function Dashboard() {
   };
   return (
     <Dialog>
-      <header className="flex z-[999] items-center border-b-2 pb-4 backdrop-blur-xl fixed top-0 left-0 right-0 pl-[4rem] pr-[4rem] p-4 dark justify-between">
-        <div className="flex items-center justify-between w-full">
-          <span className="text-[1.5rem] tracking-wide font-bold">
-            AnonVerify
-          </span>
-          <div className="flex gap-4">
-            <Link to={"/profile?action=upload"}>
-              <Button className="flex gap-2">
-                <Upload />
-                Upload Files
-              </Button>
-            </Link>
-            <Link to="/profile">
-              <Button variant="outline" className="flex gap-2">
-                <UserRoundCog />
-                Your Profile
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </header>
       <div className="min-h-screen  h-screen w-screen overflow-x-hidden flex items-center gap-[4rem]  justify-center min-w-screen">
         <div
           className={cn(
